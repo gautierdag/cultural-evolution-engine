e050ec1d42e8a24c16bff23d0f5b8c1009a04d78
Gautier Dagan <gautierdagan2017@u.northwestern.edu>
2019-04-01 20:54:27 +0200

    Fix bug

--- model/Trainer.py
+++ model/Trainer.py
@@ -18,6 +18,8 @@ class Trainer(nn.Module):
         """
         batch_size, max_len = messages.shape[0], messages.shape[1]
 
+        m = messages.clone()
+
         mask = torch.arange(max_len, device=device).expand(
             len(seq_lengths), max_len
         ) < seq_lengths.unsqueeze(1)
@@ -25,11 +27,12 @@ class Trainer(nn.Module):
         if self.training:
             mask = mask.type(dtype=messages.dtype)
             messages = messages * mask.unsqueeze(2)
-            # give full probability to eos tag (used as padding in this case)
-            messages[:, :, self.sender.eos_id] = 1.0
+            # give full probability (1) to eos tag (used as padding in this case)
+            messages[:, :, self.sender.eos_id] += (mask == 0).type(dtype=messages.dtype)
         else:
             # fill in the rest of message with eos
             messages = messages.masked_fill_(mask == 0, self.sender.eos_id)
+
         return messages
 
     def forward(self, target, distractors, tau=1.2):
