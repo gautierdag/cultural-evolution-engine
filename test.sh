505f51a8e5871d5e19956943acce92a176903319
Gautier Dagan <gautierdagan2017@u.northwestern.edu>
2019-05-09 20:48:07 +0200

    remove redundant test bash file

--- test.sh
+++
@@ -1,9 +0,0 @@
-
-# cultural/bio evolution: population size experiments
-python main.py --population-size 4 & python main.py --population-size 8 & python main.py --evolution --population-size 4 & python main.py --evolution --population-size 8
-
-python main.py --population-size 16 & python main.py --population-size 32 & python main.py --evolution --population-size 16 & python main.py --evolution --population-size 32
-
-# testing different cultural sampling methods 
-python main.py --culling-mode best & python main.py --culling-mode age
-
